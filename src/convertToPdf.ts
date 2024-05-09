1f36ddc50105f63641e845136b9c064a37f38074
Ruben Aguirre <rubenaguirrelizcano@gmail.com>
2024-05-08 19:11:30 -0600

    Made convertToPdf function. Implementing libreoffice-convert to do it.

---
+++ src/convertToPdf.ts
@@ -0,0 +1,22 @@
+import {exec} from 'child_process';
+import path from 'path';
+
+type ConvertCallback = (error: Error | null, outputPath?: string) => void;
+
+function convertToPDF(filePath: string, callback:ConvertCallback) {
+    const outputPath = filePath.replace(/\.\w+$/, '.pdf');  // Change the file extension to .pdf
+    const command = `libreoffice --headless --convert-to pdf --outdir ${path.dirname(filePath)} ${filePath}`;
+
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error: ${error.message}`);
+            return callback(error);
+        }
+        if (stderr) {
+            console.error(`Stderr: ${stderr}`);
+            return callback(new Error(stderr));
+        }
+        console.log(`stdout: ${stdout}`);
+        callback(null, outputPath);
+    });
+}
